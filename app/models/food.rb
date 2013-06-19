28a657e8204500083517fdee39852333f7017f1d
Chanisa Suwannarang <chanisas@swiftlet-008.local>
2013-06-19 18:01:37 +0700

    CHange code

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
   class Food < ActiveRecord::Base
     belongs_to :category
 
-    has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+    has_attached_file :photo, :styles => { :medium => "300x180#", :thumb => "100x50#" }
 end
