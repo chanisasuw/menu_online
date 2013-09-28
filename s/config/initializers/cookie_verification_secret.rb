fdc2377fe7bfffb060e385a1b37cdcc4ec90b55d
chanisa <chanisasuw@gmail.com>
2013-09-28 13:24:28 +0700

    delete s folder

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '349d575ed2fd9ee3703604cd0c39a518c874271f326e2adecf062ea27f3e415100d2455a64a7471027c47750471da832c3c2be9739bfad541a857b853a698fa1';
